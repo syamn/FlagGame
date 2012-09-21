6624ff96ce51b4ca718e90f8fd351b7d76b235a5
syamn <admin@sakura-server.net>
2012-09-22 01:30:12 +0900

    Add testing, Stage class.

---
+++ src/main/java/syam/FlagGame/Game/Stage.java
@@ -0,0 +1,13 @@
+/**
+ * FlagGame - Package: syam.FlagGame.Game
+ * Created: 2012/09/22 1:29:14
+ */
+package syam.FlagGame.Game;
+
+/**
+ * Stage (Stage.java)
+ * @author syam(syamn)
+ */
+public class Stage {
+
+}
