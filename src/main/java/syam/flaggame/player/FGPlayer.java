20a6b21e3268838c18aea6e837998c5dc1b1a230
syamn <admin@sakura-server.net>
2012-10-05 23:25:58 +0900

    Add way to get joined game by player instance.

--- src/main/java/syam/flaggame/player/FGPlayer.java
+++ src/main/java/syam/flaggame/player/FGPlayer.java
@@ -5,6 +5,7 @@ import java.util.logging.Logger;
 import org.bukkit.entity.Player;
 
 import syam.flaggame.FlagGame;
+import syam.flaggame.game.Game;
 
 public class FGPlayer {
 	// Logger
@@ -16,6 +17,9 @@ public class FGPlayer {
 	private Player player;
 	private PlayerProfile profile;
 
+	// 参加中のゲーム
+	private Game game = null;
+
 	/**
 	 * コンストラクタ
 	 * @param player
@@ -26,6 +30,14 @@ public class FGPlayer {
 	}
 
 	/* getter / setter */
+	public void setPlayingGame(Game game){
+		this.game = game;
+	}
+	public Game getPlayingGame(){
+		return this.game;
+	}
+
+
 	public Player getPlayer(){
 		return this.player;
 	}
